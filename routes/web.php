3d322729bef5d6a522274303fe95468494c07cdb
ryousuke.mizokami <ming10r1170@gmail.com>
2023-05-15 22:06:41 +0000

    follow, unfollow

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\MicropostsController;
+use App\Http\Controllers\UserFollowController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,6 +31,14 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 Route::group(['middleware' => ['auth']], function () {                                    // 追記
+
+    Route::group(['prefix' => 'users/{id}'], function () {                                          // 追記
+        Route::post('follow', [UserFollowController::class, 'store'])->name('user.follow');         // 追記
+        Route::delete('unfollow', [UserFollowController::class, 'destroy'])->name('user.unfollow'); // 追記
+        Route::get('followings', [UsersController::class, 'followings'])->name('users.followings'); // 追記
+        Route::get('followers', [UsersController::class, 'followers'])->name('users.followers');    // 追記
+    });
+
     Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);     // 追記
     Route::resource('microposts', MicropostsController::class, ['only' => ['store', 'destroy']]);     // 追記
 });
